e6e36f97b525c8bab2d65baac27246cd34605d15
xiaoliu <2124619132@qq.com>
2023-01-09 12:24:38 +0800

    Create upload_files.py

---
+++ upload_files.py
@@ -0,0 +1,6 @@
+def upload():
+    # Prompt user to upload song and MIDI file
+    song_file = input('Please upload your song file: ')
+    midi_file = input('Please upload your MIDI file: ')
+
+    return song_file, midi_file
